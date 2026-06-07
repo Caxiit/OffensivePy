b38810ba1967880b3f278de1ea30191ae7dab1c7
Joseph Bravo <caxitdestroy@protonmail.com>
2026-06-07 00:53:58 -0400

    feat: agregar ejercicio de condicionales

---
+++ ejercicios/basico/03_condicionales.py
@@ -0,0 +1,10 @@
+# Ejercicio básico: condicionales
+
+nota = float(input("Ingresa tu nota: "))
+
+if nota >= 4.0:
+    print("Aprobaste")
+elif nota >= 3.5:
+    print("Estás cerca, puedes mejorar")
+else:
+    print("Reprobaste, toca estudiar más")
